cf5983ac843d8c213fa44217113fc8716904df96
KaSaMa <kayrosama@gmail.com>
2024-01-30 16:36:55 -0600

    Delete config directory

--- config/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://kohana:Kinteki#69@127.0.0.1:3306/ksmdb"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-conn = engine.connect()
-
-metadata = MetaData()
